c0fe001fd03164ace9edc177703bb39e1ea03412
Toldry <toldry@gmail.com>
2020-06-10 14:23:08 +0300

    added prohibited phrases check

--- listener.py
+++ listener.py
@@ -8,17 +8,20 @@ import racb_db
 
 def handle_incoming_comment(comment):
     logging.debug(f'Handling a comment: {comment.permalink}')
+    other_subreddit = check_pattern(comment)
+    if other_subreddit is None:
+        return
+
     if is_mod_post(comment):
         return
 
     if not is_top_level_comment(comment):
         return
-
-    other_subreddit = check_pattern(comment)
-    if other_subreddit is None:
+        
+    if other_subreddit.lower() == comment.subreddit.title.lower():
         return
 
-    if other_subreddit.lower() == comment.subreddit.title.lower():
+    if title_contains_prohibited_phrases(comment):
         return
 
     logging.debug(f'Match found: {comment.permalink}')
@@ -31,10 +34,17 @@ def is_mod_post(comment):
 
 def is_top_level_comment(comment):
     return comment.parent_id == comment.link_id
+    
+prohibited_phrases = ['sub', 'subs', 'subreddit', 'subreddits']
+prohibited_phrases = [(r'\b' + x + r'\b') for x in prohibited_phrases]
+prohibited_phrases = '|'.join(prohibited_phrases)
+prohibited_phrases = re.compile(prohibited_phrases, flags=re.IGNORECASE)
 
+def title_contains_prohibited_phrases(comment):
+    title = comment.submission.title
+    return prohibited_phrases.search(title) is not None
 
 subreddit_regex = re.compile(r'^(/)?r/([a-zA-Z0-9-_]+)$')  # compile once
-
 
 # Checks if the comment's body contains only a reference to a subreddit,
 # and return the subreddit name if there's a match
