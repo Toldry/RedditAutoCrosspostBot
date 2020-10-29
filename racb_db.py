773d101bd3ba7f38c07b7cf32ed2873096727c30
Toldry <toldry@gmail.com>
2020-10-29 06:20:03 +0200

    removed migration code

--- racb_db.py
+++ racb_db.py
@@ -7,6 +7,8 @@ import time
 import psycopg2
 import psycopg2.extras
 
+import environment
+
 def add_comment(comment):
     with conn.cursor() as cur:
         permalink = comment.permalink
@@ -37,26 +39,6 @@ def instantiate_database():
         conn.commit()
 
 DATABASE_URL = os.environ['DATABASE_URL']
-conn = psycopg2.connect(dsn=os.environ['DATABASE_URL'], sslmode='require')
+sslmode = None if environment.DEBUG else 'require'
+conn = psycopg2.connect(dsn=os.environ['DATABASE_URL'], sslmode=sslmode)
 instantiate_database()
-
-
-def __migrate_data(): #TODO: delete this
-    from tinydb import TinyDB, Query
-
-    db = TinyDB('db.json', sort_keys=True, indent=4, separators=(',', ': '))
-
-    comments = db.table('comments', cache_size=0)
-
-    Comment = Query()
-    with conn.cursor() as cur:
-        l = [(c['permalink'], datetime.fromtimestamp(c['scarped_time'])) for c in comments.all()]
-        cur.executemany('''
-        INSERT INTO scraped_comments(permalink, scraped_time)
-        VALUES(%s,  %s );
-        ''', l)
-        conn.commit()
-
-    print('done')
-    exit(0)
-__migrate_data()
