3ba70232ccbc96e79c27460efe171bd386f4f26d
Toldry <toldry@gmail.com>
2020-12-15 18:55:55 +0200

    fixed bug with thread pool, added max workers

--- phase2_handler.py
+++ phase2_handler.py
@@ -21,8 +21,9 @@ def filter_comments_from_db(verbose=False):
     waiting_period_seconds = pytimeparse.timeparse.timeparse(PHASE2_WAITING_PERIOD)
     comment_entries = racb_db.get_unchecked_comments_older_than(waiting_period_seconds)
     logging.info(f'Found {len(comment_entries)} unchecked comments')
-    with concurrent.futures.ThreadPoolExecutor(max_workers=1) as executor:
-        executor.map(process_comment_entry, comment_entries, verbose)
+    with concurrent.futures.ThreadPoolExecutor(max_workers=50) as executor:
+        args = [(ce,verbose) for ce in comment_entries]
+        executor.map(process_comment_entry, args)
     logging.info('Finished running phase 2 comment filter')
 
 
