30c3ead8dac17dbf8107b8f6eb80e0c42a8aca52
Toldry <toldry@gmail.com>
2021-08-06 15:40:21 +0300

    changed error handling

--- repost_detector.py
+++ repost_detector.py
@@ -8,7 +8,8 @@ import requests
 def get_reposts_in_sub(source_comment, target_subreddit):
     url = source_comment.submission.url
     posts = search_reposts(url)
-    posts_in_target_sub = [p for p in posts if p['subreddit'].lower() == target_subreddit.lower()]
+    target_subreddit = target_subreddit.lower()
+    posts_in_target_sub = [p for p in posts if p['subreddit'].lower() == target_subreddit]
     return posts_in_target_sub
     #the dict this function returns has the following structure:
     # [{'post_id': 'k6qn88', 'url': 'https://i.redd.it/0ih30efcs7361.jpg', 'shortlink': None, 'perma_link': '/r/gay_irl/comments/k6qn88/gayirl/', 'title': 'Gay🦸\\u200d♂️irl', 'dhash_v': 'ffff0000000010fe30263efdccc880009bff06ff16006110fad3988c0717fffd', 'dhash_h': '5331f0001a4f0e792a716b64cd5991d91349358d3d8d6dcfc5c735873b873f87', 'created_at': 1607106877.0, 'author': 'star37o', 'subreddit': 'gay_irl'}]
@@ -32,21 +33,20 @@ def search_reposts(url):
         logging.warn(f'Encountered error while accessing api.repostsleuth.com: {e}')
         return []
 
-    if response.status_code != HTTPStatus.OK:
-        try:
-            error_details = response.json()
-        except:
-            raise Exception(f'Encountered a problem with repostsleuth: {response.text}',
-                            f'parameters={parameters}')
+    if response.status_code == HTTPStatus.OK:
+        content = response.json()
+        posts = [x['post'] for x in content['matches']]
+        return posts
+    elif response.headers.get('content-type') == 'application/json':
+        error_details = response.json()
         if error_details['title'] == 'Invalid URL':
             # This typically happens when the URL points to a video (rather than an image)
-            return []
+            pass
         else:
-            raise Exception(f'Encountered a problem with repostsleuth: {error_details}',
-                            f'parameters={parameters}')
-            
-    
-    content = response.json()
-    posts = [x['post'] for x in content['matches']]
-    return posts
+            logging.warn(f'Encountered a problem with repostsleuth: {error_details}',
+                         f'parameters={parameters}')
+    else:
+        logging.warn(f'Encountered a problem with repostsleuth: {response.text}',
+                     f'parameters={parameters}')
+    return []
 
