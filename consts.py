efb9e9486d8c753cb09aee9b6ae4093fbc677bcc
Toldry <toldry@gmail.com>
2021-08-09 08:00:13 +0300

    fix #24

--- consts.py
+++ consts.py
@@ -1,6 +1,8 @@
 # Subs that contain a high likelyhood of having submissions that break reddit's rules go here
 # also in general subs that give me the heebie-jeebies 🤢
 SUB_BLACKLIST = [
+    'all', #Added to prevent interaction with r/all
+
     'suddenlysexoffender',
     'cringetopia',
     'ReviewsByRetards',
