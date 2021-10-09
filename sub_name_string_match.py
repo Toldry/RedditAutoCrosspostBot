93df1e879239d060d5c817eb5546da65a3b8db2e
Toldry <toldry@gmail.com>
2021-10-10 02:18:04 +0300

    change filter for fuzzy string match

--- sub_name_string_match.py
+++ sub_name_string_match.py
@@ -1,13 +1,13 @@
 from thefuzz import process
 import pandas as pd
-import math
 
 # this file is created by processing and filtering the files provided by https://frontpagemetrics.com/list-all-subreddits
 FILTERED_SUBREDDITS_FILENAME = 'subreddit_names.csv' 
+
 MINIMUM_SUBS = 5000
 MINIMUM_MATCH_SCORE = 70
 EXTRACT_LIMIT = 12
-MAX_STRING_LEN_DELTA = 3
+MAX_STRING_LEN_RATIO_DELTA = 0.4
 RESULTS_LIMIT = 4
 
 
@@ -15,11 +15,13 @@ def get_matches(subreddit_name):
     df = pd.read_csv(FILTERED_SUBREDDITS_FILENAME)
     subreddit_names = df['subreddit_name']
     extracted = process.extract(subreddit_name, subreddit_names, limit=EXTRACT_LIMIT, )
+    n = len(subreddit_name)
+
     results = [
         name for name,score,key 
-        in extracted 
+        in extracted
         if score >= MINIMUM_MATCH_SCORE
-        and abs(len(name) - len(subreddit_name)) <= MAX_STRING_LEN_DELTA
+        and (abs(len(name) - n))/n <= MAX_STRING_LEN_RATIO_DELTA
     ]
 
     results = results[:RESULTS_LIMIT]
