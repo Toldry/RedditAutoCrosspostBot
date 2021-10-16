71ba9039b359aa39663f264ffa66d238b36d950a
Toldry <toldry@gmail.com>
2021-10-16 17:44:31 +0300

    changed subreddit_names.csv

--- sub_name_string_match.py
+++ sub_name_string_match.py
@@ -15,26 +15,15 @@ RESULTS_LIMIT = 4
 
 def get_matches(subreddit_name):
     df = pd.read_csv(FILTERED_SUBREDDITS_FILENAME) 
-    # df = pd.read_csv(FILTERED_SUBREDDITS_FILENAME, keep_default_na=False) 
     subreddit_names = df['subreddit_name']
     extracted = process.extract(subreddit_name, subreddit_names, limit=EXTRACT_LIMIT, )
     n = len(subreddit_name)
-
-
-    # delete this try block once the "'float' has no len()" bug is resolved
-    try:
-        logging.info(f'subreddit_name: {subreddit_name}')
-        names = [name for name,score,key in extracted]
-        for name in names:
-            logging.info(f'Name: {name}, name type: {type(name)}')
-    except: 
-        pass
 
     results = [
         name for name,score,key 
         in extracted
         if score >= MINIMUM_MATCH_SCORE
-        and ( (abs( len(name) - n))/n <= MAX_STRING_LEN_RATIO_DELTA)
+        and (abs( len(name) - n))/n <= MAX_STRING_LEN_RATIO_DELTA
     ]
 
     results = results[:RESULTS_LIMIT]
